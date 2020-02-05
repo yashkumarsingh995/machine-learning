ff6687225cf0f05ec7952324ec94d6f35fb5ccbf
yashkumarsingh995 <60690663+yashkumarsingh995@users.noreply.github.com>
2020-02-05 15:08:19 +0530

    Commit

--- Jarves.c
+++ Jarves.c
@@ -1,3 +1,21 @@
-1: artificial intelligence
-2:logics
-3: adaptability
+#include <stdio.h>
+#define X 365
+#define Y 366
+int main()
+{
+int n;
+printf("enter value");
+scanf("%d",&n);
+if(n>Y||n<X)
+printf("invalid number\n");
+else{
+    if(n==Y)
+    printf("leap year");
+    else
+    printf("normal year");
+    
+}
+return 0;
+
+    
+}
